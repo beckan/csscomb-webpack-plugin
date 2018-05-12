c48731598f9e8c5e35c077f61c90bd441b87c96b
Daniel Bäckström <danielbackstrom@live.se>
2018-05-12 20:33:42 +0200

    Improved structure

--- src/index.js
+++ src/index.js
@@ -18,6 +18,8 @@ class CSSCombWebpackPlugin {
 	constructor (options = {}) {
 		this.pluginName = 'CSSCombWebpackPlugin';
 		this.options = options;
+
+		this.logs = [];
 	}
 
 	/**
@@ -47,13 +49,39 @@ class CSSCombWebpackPlugin {
 		}
 	}
 
+	initCSSComb () {
+		if (!this.csscomb) {
+			this.csscomb = new CSSComb();
+		}
+
+		this.loadCSSCombConfig();
+	}
+
+	loadCSSCombConfig () {
+		// Get CSSComb config path
+		const configPath = path.resolve(this.options.configFile);
+		let config = null;
+
+		// If a custom config exists, then get it
+		if (fs.existsSync(configPath)) {
+			this.logs.push(`Using custom config file "${configPath}"...\n`);
+			config = JSON.parse(fs.readFileSync(path.resolve(this.options.configFile), 'utf8'));
+		}
+		// Otherwise get csscomb stadard config
+		else {
+			this.logs.push('Using default config file...\n');
+			config = CSSComb.getConfig('csscomb');
+		}
+
+		this.csscomb.configure(config);
+	}
+
 	/**
 	 * Process files
 	 * @param  {[Object]} compiler [Webpack compiler]
 	 * @param  {Function} callback [Webpack callback function]
 	 */
 	run (compiler, callback) {
-		const context = this.options.context || compiler.context;
 
 		// Merge options
 		this.options = Object.assign({
@@ -61,41 +89,21 @@ class CSSCombWebpackPlugin {
 			displayErrors: true,
 			files: '**/*.s?(c|a)ss'
 		}, this.options);
-
-		let logs = [];
 		let touched = false;
 
 		// Start message
-		logs.push(chalk.bgBlackBright.black('   CSSComb is processing files:   \n'));
+		this.logs.push(chalk.bgBlackBright.black('   CSSComb is processing files:   \n'));
 
-		// Get CSSComb config path
-		const configPath = path.resolve(this.options.configFile);
-		let config = null;
-
-		// If a custom config exists, then get it
-		if (fs.existsSync(configPath)) {
-			logs.push(`Using custom config file "${configPath}"...\n`);
-			config = JSON.parse(fs.readFileSync(path.resolve(this.options.configFile), 'utf8'));
-		}
-		// Otherwise get csscomb stadard config
-		else {
-			logs.push('Using default config file...\n');
-			config = CSSComb.getConfig('csscomb');
-		}
-
-		// New instans of CSSComb
-		const csscomb = new CSSComb();
+		// Initilaize CSSComb
+		this.initCSSComb();
 
 		// Get all files to process
 		globby(this.options.files).then(paths => {
 
-			// Configure CSSComb
-			csscomb.configure(config);
-
-			let promises = [];
+			this.promises = [];
 
 			// Walk trough files
-			paths.forEach((path) => {
+			paths.forEach(path => {
 				let thisPromies = new Promise((resolve, reject) => {
 
 					// Read file
@@ -105,7 +113,7 @@ class CSSCombWebpackPlugin {
 						let css = data.toString();
 
 						// Let CSSComb process the content
-						csscomb.processString(css).then((processedData) => {
+						this.csscomb.processString(css).then(processedData => {
 							// If the file is already processed. Then resolve without write to the file
 							if (css === processedData) {
 								resolve();
@@ -114,19 +122,19 @@ class CSSCombWebpackPlugin {
 
 							// Write changes to the file, then log success message and resolve
 							fs.writeFile(path, processedData, () => {
-								logs.push(`${chalk.greenBright('[success]')} ${path}`);
+								this.logs.push(`${chalk.greenBright('[success]')} ${path}`);
 								touched = true;
 								resolve();
 							});
 
 						// On CSSComb process error
-						}, (reason) => {
+						}, reason => {
 
 							// If we want to display CSSComb errors, then log the error
 							if (this.options.displayErrors) {
 								let error = reason.stack || '';
-								logs.push(`${chalk.redBright('[failed]')} ${path}`);
-								logs.push(`${chalk.underline.whiteBright('Message')}: "${error}"\n`);
+								this.logs.push(`${chalk.redBright('[failed]')} ${path}`);
+								this.logs.push(`${chalk.underline.whiteBright('Message')}: "${error}"\n`);
 								touched = true;
 							}
 							resolve();
@@ -135,16 +143,16 @@ class CSSCombWebpackPlugin {
 				});
 
 				// Push promise to the premise array
-				promises.push(thisPromies);
+				this.promises.push(thisPromies);
 			});
 
 			//When all files is processed
-			Promise.all(promises).then(() => {
-				logs.push(chalk.bgBlackBright.black('\n   CSSComb is done processing files.   \n'));
+			Promise.all(this.promises).then(() => {
+				this.logs.push(chalk.bgBlackBright.black('\n   CSSComb is done processing files.   \n'));
 
 				// Console log all messages if some file where touched
 				if (touched === true) {
-					logs.forEach((message) => {
+					this.logs.forEach(message => {
 						console.log(message);
 					});
 				}
